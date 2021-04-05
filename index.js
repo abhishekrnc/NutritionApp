3003e55a114204dc87e3fd2fd489c224b75b636a
Abhishek <abhishektupudana@gmail.com>
2021-04-05 17:55:06 +0530

    updted

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const mongoose=require('mongoose');
 
 const app=express();
 
+// schema for foods collection
+
+const foodSchema=new.mongoose.Schema({
+    name:String,
+    calories:Number,
+    protein:Number,
+    carbs:Number,
+    fats:Number,
+    fiber:Number,
+    weight:Number,
+})
+
+//Mongo Connection
 mongoose.connect("mongodb://127.0.0.1:27017",{  //To connect MongoDb with the file
     useNewUrlParser:true,
     useUnifiedTopology:true
