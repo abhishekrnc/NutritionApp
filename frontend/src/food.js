042b91466f8dafdaf1d6fb7ab8db518c52037273
Abhishek <abhishektupudana@gmail.com>
2021-04-07 17:38:40 +0530

    updated

--- frontend/src/food.js
+++ frontend/src/food.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Food extends Component {
+
+    componentDidMount(){
+
+        fetch("http://localhost:8000/foods")
+        .then((response=>response.json()))
+        .then((foods)=>{
+            console.log(foods);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+
+    }
+
     render() {
         return (
             <div className="container">
-                <div className="form-group">
-                    <input className="form-control"/>
+                <div className="form-group" style={{marginTop:"30px"}}>
+                    <input className="form-control" placeholder="Search Food"/>
                 </div>
             </div>
         )
